a76ae1e8295a7eda3aa60c6eab4b217f696888bb
Matt Aussaguel <matthieu.aussaguel@gmail.com>
2016-09-20 16:59:00 +0100

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#welcome'
 end
