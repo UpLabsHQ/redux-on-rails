5dfcc373f954dec3b626fcdaebb5b3069b11017c
Matt Aussaguel <matthieu.aussaguel@gmail.com>
2016-10-07 16:15:47 +0200

    Remove extra line

--- config/initializers/webpack-rails-react.rb
+++ config/initializers/webpack-rails-react.rb
@@ -1,7 +1,6 @@
 require 'webpack/rails/react_server_renderer'
 
 # Watch our webpack files for changes in dev, so we can reload the JS VMs with the new JS code.
-
 app = Rails.application
 app.config.react.server_renderer = Webpack::Rails::ReactServerRenderer
 app.config.watchable_files.concat Dir["#{app.root}/app/assets/javascripts/**/*"]
